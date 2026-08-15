71d82f201ba6225681320ba00966c3dcae7d8826
chaukz <reboningochauke@gmail.com>
2026-08-16 01:06:35 +0200

    more math operations

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <vector>
-
-//typedef std::vector<std::pair<std::string, int>> pairList_t;
-//typedef std::string text_t;
-//typedef int number_t;
-
-using text_t = std::string;
-using number_t = int;
+#include <cmath>
 
 int main(){
-std::string name ;
-int age ;
-std::cout << "Enter your name: ";
-std::getline(std::cin >> std::ws, name); // Use getline to allow spaces in the name
+double x = 3;
+double y = 4;
+double z;
 
-std::cout << "Enter your age: ";
-std::cin >> age;
-
-std::cout << "Hello, " << name << "!" << std::endl;
-std::cout << "You are " << age << " years old." << std::endl;
+z = sqrt(9);
+std::cout << "The square root of 9 is: " << z << std::endl;
 return 0;
 }   
