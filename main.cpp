9b587ae39fcde2c5e9ab9be15eb74c813864e7a4
chaukz <reboningochauke@gmail.com>
2026-08-16 14:55:09 +0200

    switch case lesson

--- main.cpp
+++ main.cpp
@@ -4,17 +4,48 @@
 
 int main()
 {
-    int age ;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    if  (age < 0) {
-        std::cout << "Age cannot be negative." << std::endl;
-    } else if (age < 18) {
-        std::cout << "You are a minor." << std::endl;
-    } else if (age < 65) {
-        std::cout << "You are an adult." << std::endl;
-    } else {
-        std::cout << "You are a senior citizen." << std::endl;
+    int month;
+    std::cout << "Enter a month number (1-12): ";
+    std::cin >> month;
+    switch(month){
+case 1:
+        std::cout << "January" << std::endl;
+        break;
+    case 2:
+        std::cout << "February" << std::endl;
+        break;
+    case 3:     
+        std::cout << "March" << std::endl;
+        break;
+    case 4:
+        std::cout << "April" << std::endl;
+        break;
+    case 5:
+        std::cout << "May" << std::endl;
+        break;
+    case 6:
+        std::cout << "June" << std::endl;
+        break;
+    case 7:
+        std::cout << "July" << std::endl;
+        break;
+    case 8:
+        std::cout << "August" << std::endl;
+        break;
+    case 9:
+        std::cout << "September" << std::endl;
+        break;
+    case 10:        
+        std::cout << "October" << std::endl;
+        break;
+    case 11:
+        std::cout << "November" << std::endl;
+        break;
+    case 12:
+        std::cout << "December" << std::endl;
+        break;
+    default:
+        std::cout << "Invalid month" << std::endl;
     }
     return 0;
 }
