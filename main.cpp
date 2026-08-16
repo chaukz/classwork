708e16982ac7a3deee2d2f563739899fc76d3ff7
chaukz <reboningochauke@gmail.com>
2026-08-16 14:50:23 +0200

    if statements lesson

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 
 int main()
 {
-    double x, y, z;
-
-    std::cout << "Enter X Value: ";
-    std::cin >> x;
-
-    std::cout << "Enter Y Value: ";
-    std::cin >> y;
-
-    z = sqrt(pow(x, 2) + pow(y, 2));
-
-    std::cout << "The value of Z is: " << z << std::endl;
+    int age ;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    if  (age < 0) {
+        std::cout << "Age cannot be negative." << std::endl;
+    } else if (age < 18) {
+        std::cout << "You are a minor." << std::endl;
+    } else if (age < 65) {
+        std::cout << "You are an adult." << std::endl;
+    } else {
+        std::cout << "You are a senior citizen." << std::endl;
+    }
+    return 0;
 }
