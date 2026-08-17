58383f5878f87090b89dc9e24fd251f11b5b18dc
chaukz <reboningochauke@gmail.com>
2026-08-17 21:27:03 +0200

    String methods lesson

--- main.cpp
+++ main.cpp
@@ -4,23 +4,13 @@
 
 int main()
 {
-    double temp;
-    char unit;
-    std::cout << "Enter temperature (e.g., 32 F or 0 C): ";
-    std::cin >> temp >> unit;
-    if (unit == 'F' || unit == 'f')
-    {
-        double celsius = (temp - 32) * 5.0 / 9.0;
-        std::cout << temp << " F is " << celsius << " C" << std::endl;
-    }
-    else if (unit == 'C' || unit == 'c')
-    {
-        double fahrenheit = (temp * 9.0 / 5.0) + 32;
-        std::cout << temp << " C is " << fahrenheit << " F" << std::endl;
-    }
-    else
-    {
-        std::cout << "Invalid unit. Please enter 'F' for Fahrenheit or 'C' for Celsius." << std::endl;
+ std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    if (name.length() > 12){
+        std::cout << "Name is too long. Please enter a name with 12 characters or fewer." << std::endl;
+    } else {
+        std::cout << "Hello, " << name << "!" << std::endl;
     }
     return 0;
 }
