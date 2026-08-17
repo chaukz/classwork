73c459f29c7171b4160d2c1f0b89d0622968c31e
chaukz <reboningochauke@gmail.com>
2026-08-17 21:12:13 +0200

    Ternary Operator Lesson

--- main.cpp
+++ main.cpp
@@ -4,43 +4,10 @@
 
 int main()
 {
-    char op;
-    double num1, num2, result;
-
-    std::cout << "********Calculator********\n";
-    std::cout << "Enter either (+), (-), (*), (/) or (^) for the operation you want to perform: ";
-    std::cin >> op;
-
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-
-    switch(op)
-    {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        case '^':
-            result = std::pow(num1, num2);
-            break;
-        default:
-            std::cout << "Error! Operator is not correct";
-            return 1;
-    }
-
-    std::cout << "*****************************";
-    std::cout << "Result: " << result << std::endl;
-    std::cout << "*****************************";
-
+    int grade;
+    std::cout << "Enter the grade: ";
+    std::cin >> grade;
+    grade >= 40 ? std::cout << "Pass" << std::endl : std::cout << "Fail" << std::endl;
+    
     return 0;
 }
