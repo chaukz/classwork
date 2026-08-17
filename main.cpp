d4e31079f42a73bdbdfd7b27726a15826aa7154d
chaukz <reboningochauke@gmail.com>
2026-08-17 21:08:36 +0200

    calculator project lesson

--- main.cpp
+++ main.cpp
@@ -4,48 +4,43 @@
 
 int main()
 {
-    int month;
-    std::cout << "Enter a month number (1-12): ";
-    std::cin >> month;
-    switch(month){
-case 1:
-        std::cout << "January" << std::endl;
-        break;
-    case 2:
-        std::cout << "February" << std::endl;
-        break;
-    case 3:     
-        std::cout << "March" << std::endl;
-        break;
-    case 4:
-        std::cout << "April" << std::endl;
-        break;
-    case 5:
-        std::cout << "May" << std::endl;
-        break;
-    case 6:
-        std::cout << "June" << std::endl;
-        break;
-    case 7:
-        std::cout << "July" << std::endl;
-        break;
-    case 8:
-        std::cout << "August" << std::endl;
-        break;
-    case 9:
-        std::cout << "September" << std::endl;
-        break;
-    case 10:        
-        std::cout << "October" << std::endl;
-        break;
-    case 11:
-        std::cout << "November" << std::endl;
-        break;
-    case 12:
-        std::cout << "December" << std::endl;
-        break;
-    default:
-        std::cout << "Invalid month" << std::endl;
+    char op;
+    double num1, num2, result;
+
+    std::cout << "********Calculator********\n";
+    std::cout << "Enter either (+), (-), (*), (/) or (^) for the operation you want to perform: ";
+    std::cin >> op;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    switch(op)
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '^':
+            result = std::pow(num1, num2);
+            break;
+        default:
+            std::cout << "Error! Operator is not correct";
+            return 1;
     }
+
+    std::cout << "*****************************";
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "*****************************";
+
     return 0;
 }
