2e1b2538ae642c440de9a8064fbe927239520086
chaukz <reboningochauke@gmail.com>
2026-08-19 20:01:52 +0200

    sizeof lesson

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+
 
 int main()
 {
- std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    if (name.length() > 12){
-        std::cout << "Name is too long. Please enter a name with 12 characters or fewer." << std::endl;
-    } else {
-        std::cout << "Hello, " << name << "!" << std::endl;
-    }
+   std::string name = "John";
+   char grade = 'A';
+    double gpa = 3.75;
+    std::cout << "size of gpa is:" << sizeof(char) << "bytes\n";
     return 0;
 }
