90664f8d92a7059ee1de34aac9b639cff472e4e1
chaukz <reboningochauke@gmail.com>
2026-08-24 00:46:43 +0200

    credit card validator program

--- main.cpp
+++ main.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
     
-void printInfo(const std::string &name, int age);
+int getDigit( const int number);
+int sumOddDigits( const std::string cardnumber);
+int sumEvenDigits( const std::string cardnumber);
     
 int main()
 {
-    std:: string name = "John";
-    int age  =21;
-
-    printInfo(name, age);
-
+    std::string cardnumber;
+    int result =0;
+    std::cout << "Enter a card number: ";
+    std::cin >> cardnumber;
+    
+    result = sumEvenDigits(cardnumber) + sumOddDigits(cardnumber);
+    
+    if (result % 10 == 0)
+    {
+        std::cout << "The card number is valid." << std::endl;
+    }
+    else
+    {
+        std::cout << "The card number is invalid." << std::endl;
+    }   
+    std::cout << "The sum of the digits is: " << result << std::endl;
 return 0;
+
 }
-void printInfo(const std::string &name, int age)
+int getDigit( const int number)
 {
-    name = "Jane"; // This will not affect the original name in main since it's passed by const reference
-    age = 30; // This will not affect the original age in main since it's passed by value
-    std::cout << "Name: " << name << '\n'
-    <<"Age: " << age << std::endl;
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits( const std::string cardnumber){
+int sum = 0;
+    for (int i = cardnumber.length() - 1; i >= 0; i -= 2)
+    {
+        sum += cardnumber[i] - '0';
+    }
+    return sum;
+}
+int sumEvenDigits( const std::string cardnumber){
+
+    int sum = 0;
+    for (int i = cardnumber.length() - 2; i >= 0; i -= 2)
+    {
+        sum += getDigit((cardnumber[i] - '0')*2);
+    }
+    return sum;
 }
