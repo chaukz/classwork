808b156c40736a8f31a8736b80aa992b849930ef
chaukz <reboningochauke@gmail.com>
2026-08-23 21:45:59 +0200

    fill an array with user input and adding an option to quit

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@ int main() {
    
 std::string foods[5];
 int size = sizeof(foods)/sizeof(foods[0]);
+std::string temp;
 for(int i = 0; i < size; i++) {
     std::cout << "Enter food item " << (i + 1) << ": ";
-    std::getline(std::cin, foods[i]);
+    std::getline(std::cin, temp);
+    foods[i] = temp;
+    if (temp== "q")
+    {
+        /* code */break;
+    }else{
+        continue;
+    }
+    
 }
 std::cout << "\nYou entered the following food items:\n";
 for(std::string food : foods) {
