61dd76fcce345fa0c0bf371561f9cd70101be599
chaukz <reboningochauke@gmail.com>
2026-08-24 16:53:56 +0200

    Pointer Lesson

--- main.cpp
+++ main.cpp
@@ -6,43 +6,21 @@ int sumEvenDigits( const std::string cardnumber);
     
 int main()
 {
-    std::string cardnumber;
-    int result =0;
-    std::cout << "Enter a card number: ";
-    std::cin >> cardnumber;
-    
-    result = sumEvenDigits(cardnumber) + sumOddDigits(cardnumber);
-    
-    if (result % 10 == 0)
+
+    std::string name = "John Doe";
+    int age = 30;
+std::string freePizzas[5] = {"Margherita", "Pepperoni", "Hawaiian", "Veggie", "BBQ Chicken"};
+
+    std::string *pname = &name;
+    int *page = &age;
+    std::string *pfreePizzas = freePizzas;
+
+    std::cout << "Name: " << *pname << std::endl;
+    std::cout << "Age: " << *page << std::endl;
+    std::cout << "Free Pizzas: " << std::endl;
+    for (int i = 0; i < 5; i++)
     {
-        std::cout << "The card number is valid." << std::endl;
+        std::cout << " - " << pfreePizzas[i] << std::endl;
     }
-    else
-    {
-        std::cout << "The card number is invalid." << std::endl;
-    }   
-    std::cout << "The sum of the digits is: " << result << std::endl;
 return 0;
-
-}
-int getDigit( const int number)
-{
-    return number % 10 + (number / 10 % 10);
-}
-int sumOddDigits( const std::string cardnumber){
-int sum = 0;
-    for (int i = cardnumber.length() - 1; i >= 0; i -= 2)
-    {
-        sum += cardnumber[i] - '0';
-    }
-    return sum;
-}
-int sumEvenDigits( const std::string cardnumber){
-
-    int sum = 0;
-    for (int i = cardnumber.length() - 2; i >= 0; i -= 2)
-    {
-        sum += getDigit((cardnumber[i] - '0')*2);
-    }
-    return sum;
 }
