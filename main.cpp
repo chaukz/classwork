8b7bf358dfab5ecd8b256a848c805cbcdb2b60b6
chaukz <reboningochauke@gmail.com>
2026-08-25 22:54:02 +0200

    iterative version

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 void walk(int steps);
-int main()
-{
-    walk(100);
+int main() {
+    int steps;
+    std::cout << "Enter the number of steps to walk: ";
+    std::cin >> steps;
+    walk(steps);
     return 0;
 }
 
-void walk(int steps)
-{
-    if(steps > 0)
-    {
-        std::cout << "Walking " << steps << " steps." << std::endl;
-        walk(steps - 1);
+void walk (int steps) {
+    for (int i = 0; i < steps; ++i) {
+        std::cout << "Step " << (i + 1) << std::endl;
     }
 }
